83ef52f6ab1cd60bf0f4478eecb57c7d27a5a0ac
Brian Wilson <briwils2@rcdn-briwils2-8815.cisco.com>
2016-12-12 20:58:55 -0600

    Adding Sickrage Scripts for installing and managing on Drobo

--- Drobo/sickrage/preflight.sh
+++ Drobo/sickrage/preflight.sh
@@ -2,7 +2,7 @@ cd /mnt/DroboFS/Shares/DroboApps
 mkdir sickrage
 cd sickrage/
 rm -r app
-git clone https://github.com/SickRage/SickRage.git app
+/mnt/DroboFS/Shares/DroboApps/git/bin/git clone https://github.com/SickRage/SickRage.git app
 mkdir data var
 mkdir data/Logs
 cp /tmp/sickrage/Drobo/sickrage/service.sh .
